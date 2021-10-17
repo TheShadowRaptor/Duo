c3889d6f3cdc0481f362ce3361186ca48b510a99
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2021-10-16 21:56:56 -0300

    Start Of Orginization And fixed Camera Offset
    Camera was off a little bit causing one arm to look longer then the other.

---
+++ Assets/Scripts/D103/D103Universal.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D103Universal : MonoBehaviour
+{
+    public GameObject cameraBlock;
+    public float turningSpeed = 0;
+    public float movingSpeed = 0;
+
+    public bool moveL = true;
+    public bool moveR = true;
+
+    private Rigidbody rb;
+
+    private float xRotate;
+    private float yRotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("left") && moveL == (true))
+        {
+            transform.Translate(-0.07f, 0, 0);
+        }
+
+        if (Input.GetKey("right") && moveR == (true))
+        {
+            transform.Translate(0.07f, 0, 0);
+        }
+
+        xRotate = cameraBlock.transform.eulerAngles.x;
+
+        if (xRotate <= 70f || xRotate >= 360)
+        {
+            if (Input.GetKey("up"))
+            {
+                xRotate += -turningSpeed;
+            }
+
+            if (Input.GetKey("down"))
+            {
+                xRotate += turningSpeed;
+            }
+        }
+
+        else if (xRotate > 70f && xRotate < 180f)
+        {
+            xRotate = 69.9f;
+        }
+
+        else if (xRotate < 360f && xRotate > 180f)
+        {
+            xRotate = 360.1f;
+        }
+
+        cameraBlock.transform.rotation = Quaternion.Euler(xRotate, yRotate, 0f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall") && Input.GetKey("left"))
+        {
+            moveL = false;
+        }
+        if (collision.gameObject.CompareTag("Wall") && Input.GetKey("right"))
+        {
+            moveR = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        moveL = true;
+        moveR = true;
+    }
+}
+
