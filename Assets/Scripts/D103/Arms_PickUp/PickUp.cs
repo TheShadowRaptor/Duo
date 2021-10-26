1e2c5c01cba2e6d5fc90d1dae0945912c4c57405
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2021-10-26 18:25:11 -0300

    Bubbles for acid Created

--- Assets/Scripts/D103/Arms_PickUp/PickUp.cs
+++ Assets/Scripts/D103/Arms_PickUp/PickUp.cs
@@ -10,7 +10,7 @@ public class PickUp : MonoBehaviour
     public Material PickUpMaterial;
     public Material NormalMaterial;
 
-    public Rigidbody rb;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
